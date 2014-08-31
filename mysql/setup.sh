a2b27c51fbee34e8af96b1f7417012170d9b9c73
Adrien Samson <adrien@kyklydse.com>
2014-08-31 15:24:54 +0200

    Add mysql

---
+++ mysql/setup.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+if [ -z "$(ls /var/lib/mysql)" ]
+then
+    mysql_install_db
+    /etc/init.d/mysql start
+    echo "GRANT ALL ON *.* TO root@'%';" | mysql
+    MAINT_PASSWORD=$(grep -m 1 password /etc/mysql/debian.cnf | awk '{print $3;}')
+    echo "GRANT ALL ON *.* TO 'debian-sys-maint'@'localhost' IDENTIFIED BY '$MAINT_PASSWORD';" | mysql
+    /etc/init.d/mysql stop
+fi
+
